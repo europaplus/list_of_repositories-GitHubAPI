46bea530437507251cd10f64c149c0358624fc28
europaplus <basshuntep@mail.ru>
2021-08-26 13:57:27 +0300

    end hw-1

--- src/shared/store/ApiStore.tsx
+++ src/shared/store/ApiStore.tsx
@@ -1,4 +1,5 @@
-import {ApiResponse, IApiStore, RequestParams} from "./types";
+import {ApiResponse, HTTPMethod, IApiStore, RequestParams} from "./types";
+import qs from 'qs';
 
 export default class ApiStore implements IApiStore {
     // TODO: реализовать
@@ -8,9 +9,28 @@ export default class ApiStore implements IApiStore {
         this.baseUrl = url;
     }
 
+    private getRequestData<ReqT>(params: RequestParams<ReqT>): [string, RequestInit] {
+        let endpoint = `${this.baseUrl}${params.endpoint}`;
+        const req: RequestInit = {
+            headers: {...params.headers},
+        };
+
+        if (params.method === HTTPMethod.GET) {
+            endpoint = `${endpoint}?${qs.stringify(params.data)}`
+        }
+        if (params.method === HTTPMethod.POST) {
+            req.body = JSON.stringify(params.data);
+            req.headers = {
+                ...req.headers,
+                ['Content-Type']: 'application/json;charset=utf-8',
+            };
+        }
+        return [endpoint, req];
+    }
+
     async request<SuccessT, ErrorT = any, ReqT = {}>(params: RequestParams<ReqT>): Promise<ApiResponse<SuccessT, ErrorT>> {
         try {
-            const response = await fetch(`${this.baseUrl}${params.endpoint}`);
+            const response = await fetch(...this.getRequestData(params));
             const data = await response.json();
             return {
                 success: response.ok,
